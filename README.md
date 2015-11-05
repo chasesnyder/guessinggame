5f38604eaf1b3aa72f730c29d254806ef2d54851
Chase <chase.wade.snyder@gmail.com>
2015-11-04 18:37:56 -0800

    add some features to my guessing game and a joke to my README file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # guessinggame
 CF Guessing Game
+
+This is a guessing game where you get to learn some fun facts about me, Chase Snyder. This is a project I'm doing as a part of the CodeFellows Foundations 1 course.
+
+Whats the worst thing about sending a joke in TCP? 
+I’ll keep telling it slower and slower until you get it.
---
+++ javascript/game2.js
@@ -0,0 +1,39 @@
+var answer1 = prompt("Where do you think I'm from?");
+console.log(answer1);
+if (answer1 === "Denver") {
+	alert("Yes! You got it right!");
+
+} else {
+	alert("Uh...nice try I guess. The answer was Denver. Next Question");
+}
+
+var answer2 = prompt("What's my favorite beer?");
+console.log(answer2);
+
+if (answer2 === "Fat Tire") {
+	alert("You got it right! Look at the big brain on you.");
+} else {
+	alert("What? No. Next question! (P.S., Fat Tire was the answer.)");
+}
+
+var answer3 = prompt("Where do you think I live now? Hint, it is a neighborhood in Seattle.");
+console.log(answer3);
+
+if (answer3 === "Greenwood") {
+	alert("Yes! You got it right. Real Einstein over here.");
+} else {
+	alert("Wow. You don't know me that well at all. I'm from Denver, my favorite beer is Fat Tire, and I live in Greenwood now.  Make sure to remember that stuff for next time. Let's try one more question.");
+}
+
+var numberOfBalls = prompt("How many balls can I juggle?");
+console.log("You think I can juggle " + numberOfBalls + " ?!?!?!");
+
+if (numberOfBalls == 3) {
+	alert("Nailed it! Nice work.");
+} else if (numberOfBalls < 3) {
+	alert("You don't think much of my juggling skills, huh?");
+} else if (numberOfBalls > 3) {
+	alert("I'm flattered, but that's a bit high.")
+} else {
+	alert("Well...I'm not even sure what you mean by that guess, but nice work anyway. Thanks for playing. Bye!")
+}
